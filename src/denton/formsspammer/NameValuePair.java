f64918440728c379b16a01e3fb2c7febd45ff6d6
Denton-L <liu.denton+github@gmail.com>
2015-11-22 04:56:18 -0500

    Created NameValuePair

---
+++ src/denton/formsspammer/NameValuePair.java
@@ -0,0 +1,10 @@
+package denton.formsspammer;
+public class NameValuePair {
+	public final String name;
+	public final Object value;
+	
+	public NameValuePair(String name, Object value) {
+		this.name = name;
+		this.value = value;
+	}
+}
