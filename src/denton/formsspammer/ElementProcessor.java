fd16a479d5a8c5eeacde04f259b0aa9aeb6df942
Denton-L <liu.denton+github@gmail.com>
2015-11-22 21:08:32 -0500

    Created ElementProcessor

---
+++ src/denton/formsspammer/ElementProcessor.java
@@ -0,0 +1,59 @@
+package denton.formsspammer;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ElementProcessor {
+	
+	private static class CheckboxElement {
+		public final String name;
+		public final String value;
+		
+		public CheckboxElement(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
+	}
+	
+	
+	private Set<String> texts = new HashSet<>();
+	private Set<String> textareas = new HashSet<>();
+	private Map<String, Set<String>> radios = new HashMap<>();
+	private Set<CheckboxElement> checkboxes = new HashSet<>();
+	private Set<String> dates = new HashSet<>();
+	private Set<String> times = new HashSet<>();
+	
+	public ElementProcessor(Set<FormElement> formElements) {
+		for (FormElement formElement : formElements) {
+			switch (formElement.type) {
+			//TEXT, TEXTAREA, RADIO, CHECKBOX, DATE, TIME;
+			case TEXT:
+				texts.add(formElement.name);
+				break;
+			case TEXTAREA:
+				textareas.add(formElement.name);
+				break;
+			case RADIO:
+				if (radios.containsKey(formElement.name)) {
+					radios.get(formElement.name).add(formElement.value);
+				} else {
+					Set<String> newSet = new HashSet<>();
+					newSet.add(formElement.value);
+					radios.put(formElement.name, newSet);
+				}
+				break;
+			case CHECKBOX:
+				checkboxes.add(new CheckboxElement(formElement.name, formElement.value));
+				break;
+			case DATE:
+				dates.add(formElement.name);
+				break;
+			case TIME:
+				times.add(formElement.name);
+				break;
+			}
+		}
+	}
+}
